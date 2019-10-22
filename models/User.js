72212ee8e72523f6ee53810f923c61a9a6f52b23
Robert Olive <robert.aaron.olive@gmail.com>
2019-10-22 13:35:22 -0500

    frankensteining jwt and passport in

---
+++ models/User.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+      // Giving the User model a name of type STRING
+      name: DataTypes.STRING
+    });
+  
+    User.associate = function(models) {
+      // Associating User with Plans
+      // When an User is deleted, also delete any associated Plans
+      User.hasMany(models.Plan, {
+        onDelete: "cascade"
+      });
+    };
+  
+    return User;
+  };
+  
--- routes/api/plans.js
+++ routes/api/plans.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const plansController = require("../../controllers/plansController");
+const jwt = require("jsonwebtoken");
+const jwtVerify = require("../../config/jwt");
 
 // Matches with "/api/books"
 router.route("/")
   .get(plansController.doAThing)
+  // .get(jwtVerify.confirmToken, jwtVerify.verifyToken, plansController.findAll)
 //   .post(booksController.create);
 
 // Matches with "/api/books/:id"
